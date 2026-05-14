65d85bc9474fb9166298204bcda8e99a49b510e7
Hrishikesh Yadav G <145208329+hrishikes15@users.noreply.github.com>
2026-05-14 16:16:34 +0530

    Time: 0 ms (100.00%) | Memory: 26.3 MB (97.93%) - LeetSync

--- 2892-check-if-array-is-good/check-if-array-is-good.cpp
+++ 2892-check-if-array-is-good/check-if-array-is-good.cpp
@@ -1,25 +1,12 @@
 class Solution {
 public:
     bool isGood(vector<int>& nums) {
-        int n = nums.size();
-
         sort(nums.begin(),nums.end());
-        int val = nums[n-1];
-
-        if(val+1 != n) return false;
-
-        unordered_map<int,int> mapp;
-
-        for(int x : nums){
-            mapp[x]++;
+        int n=nums.size();
+        for(int i=0;i<n-1;i++){
+            if(nums[i]!=i+1)
+                return false;
         }
-
-        for(auto &it : mapp){
-            if(it.first != val && it.second != 1) return false;
-
-            if(mapp[val] > 2) return false;
-        }
-
-        return true;
+        return nums[n-1]==n-1;
     }
 };
