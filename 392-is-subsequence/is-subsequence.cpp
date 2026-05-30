28b58c2a7d859161ce690e0bb37921cf19fc5f84
Hrishikesh Gurrampally <145208329+hrishi1506@users.noreply.github.com>
2026-05-30 11:35:11 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (32.13%) - LeetSync

--- 392-is-subsequence/is-subsequence.cpp
+++ 392-is-subsequence/is-subsequence.cpp
@@ -1,27 +1,21 @@
 class Solution {
-    int lcs(string s1 , string s2 , int m , int n){
-        vector<vector<int>> dp(m+1 , vector<int>(n+1,0));
-
-        for(int i = 1 ;  i <= m ; i++){
-            for(int j = 1 ; j <= n ; j++){
-                if(s1[i-1] == s2[j-1]){
-                    dp[i][j] = 1 + dp[i-1][j-1];
-                }
-                else{
-                    dp[i][j] = max(dp[i-1][j] , dp[i][j-1]);
-                }
-            }
-        }
-        return dp[m][n];
-    }
-    
 public:
     bool isSubsequence(string s, string t) {
-        int m = s.size();
-        int n = t.size();
-        int len = lcs(s,t,m,n);
+        int i = 0;
+        int j = 0;
 
-         if(len == m) return true;
-         else return false;
+        int c = s.size();
+
+        while(j < t.size()){
+            if(s[i] == t[j]){
+                i++;
+                j++;
+                c--;
+            }
+            else{
+                j++;
+            }
+        }
+        return c == 0;
     }
 };
