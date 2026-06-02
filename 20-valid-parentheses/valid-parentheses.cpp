08b99b69f966496d472e59ee2fa1c4b9aed93cf7
Hrishikesh Gurrampally <145208329+hrishi1506@users.noreply.github.com>
2026-06-02 22:26:03 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (37.40%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -5,19 +5,16 @@ public:
         int n = s.size();
 
         for(int i = 0 ; i < n; i++){
-            if(s[i] == '{' || s[i] == '[' || s[i] == '('){
+            if(s[i] == '(' || s[i] == '[' || s[i] == '{'){
                 st.push(s[i]);
             }
-            else if(st.size() == 0){
-                return false;
-            }
-            else{
-                char ch = st.top();
+            else if(st.size() == 0) return false;
+            else {
+                char val = st.top();
                 st.pop();
-                if ((s[i] == ')' && ch == '(') || (s[i] == '}' && ch == '{') || (s[i] == ']' && ch == '['))continue;
-                else return false;
-            }
 
+                if(val == '(' && s[i] != ')' || val == '{' && s[i] != '}' || val == '[' && s[i] != ']') return false;
+            }
         }
         return st.size()==0;
     }
