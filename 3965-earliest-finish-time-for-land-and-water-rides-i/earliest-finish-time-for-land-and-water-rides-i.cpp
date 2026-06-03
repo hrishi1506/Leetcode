1b8d62fed6eee95a9a213a33a1446ecacb601b06
Hrishikesh Gurrampally <145208329+hrishi1506@users.noreply.github.com>
2026-06-03 15:36:30 +0530

    Time: 0 ms (100.00%) | Memory: 91.7 MB (85.66%) - LeetSync

--- 3965-earliest-finish-time-for-land-and-water-rides-i/earliest-finish-time-for-land-and-water-rides-i.cpp
+++ 3965-earliest-finish-time-for-land-and-water-rides-i/earliest-finish-time-for-land-and-water-rides-i.cpp
@@ -1,26 +1,23 @@
 class Solution {
+    int solve(vector<int>&st1 , vector<int>&end1 , vector<int>&st2 , vector<int>&end2){
+        int finish1 = INT_MAX;
+        for(int i = 0 ; i < st1.size() ; i++){
+            finish1 = min(finish1 , st1[i] + end1[i]);
+        }
+
+        int finish2 = INT_MAX;
+        for(int j = 0 ; j < st2.size() ; j++){
+            finish2 = min(finish2 , max(finish1 , st2[j]) + end2[j]);
+        }
+
+        return finish2;
+    }
 public:
     int earliestFinishTime(vector<int>& landStartTime, vector<int>& landDuration, vector<int>& waterStartTime, vector<int>& waterDuration) {
-        int mini = INT_MAX;
+        int landFirstSt = solve(landStartTime,landDuration,waterStartTime,waterDuration);
 
-        for(int i = 0 ; i < landStartTime.size() ; i++){
-            for(int j = 0 ; j < waterStartTime.size() ; j++ ){
-                
-                int stLand = landStartTime[i];
-                int endLand = stLand + landDuration[i];
-                int stWater = max(waterStartTime[j] , endLand);
-                int waterEnd = stWater + waterDuration[j];
-                mini = min(mini , waterEnd);
+        int waterStfirst = solve(waterStartTime,waterDuration,landStartTime,landDuration);
 
-
-                int stWater2 = waterStartTime[j];
-                int waterEnd2 = stWater2 + waterDuration[j];
-                int stLand2 = max(waterEnd2 , landStartTime[i]);
-                int endLand2 = stLand2 + landDuration[i];
-                mini = min(mini,endLand2);
-
-            }
-        }
-        return mini;
+        return  min(landFirstSt,waterStfirst);
     }
 };
