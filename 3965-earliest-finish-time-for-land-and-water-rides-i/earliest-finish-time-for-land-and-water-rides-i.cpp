7191976c138bf61111c502acaa8af5a3dd744f59
Hrishikesh Gurrampally <145208329+hrishi1506@users.noreply.github.com>
2026-06-03 15:23:41 +0530

    Time: 16 ms (20.08%) | Memory: 91.9 MB (26.23%) - LeetSync

--- 3965-earliest-finish-time-for-land-and-water-rides-i/earliest-finish-time-for-land-and-water-rides-i.cpp
+++ 3965-earliest-finish-time-for-land-and-water-rides-i/earliest-finish-time-for-land-and-water-rides-i.cpp
@@ -1,27 +1,26 @@
 class Solution {
 public:
-    int earliestFinishTime(vector<int>& landStartTime, vector<int>& landDuration,
-                           vector<int>& waterStartTime, vector<int>& waterDuration) {
-        int minTime = INT_MAX;
+    int earliestFinishTime(vector<int>& landStartTime, vector<int>& landDuration, vector<int>& waterStartTime, vector<int>& waterDuration) {
+        int mini = INT_MAX;
 
-        for (int i = 0; i < landStartTime.size(); ++i) {
-            for (int j = 0; j < waterStartTime.size(); ++j) {
+        for(int i = 0 ; i < landStartTime.size() ; i++){
+            for(int j = 0 ; j < waterStartTime.size() ; j++ ){
                 
-                int startLand = landStartTime[i];
-                int endLand = startLand + landDuration[i];
-                int startWater = max(waterStartTime[j], endLand);
-                int endWater = startWater + waterDuration[j];
-                minTime = min(minTime, endWater);
+                int stLand = landStartTime[i];
+                int endLand = stLand + landDuration[i];
+                int stWater = max(waterStartTime[j] , endLand);
+                int waterEnd = stWater + waterDuration[j];
+                mini = min(mini , waterEnd);
 
-                
-                int startWater2 = waterStartTime[j];
-                int endWater2 = startWater2 + waterDuration[j];
-                int startLand2 = max(landStartTime[i], endWater2);
-                int endLand2 = startLand2 + landDuration[i];
-                minTime = min(minTime, endLand2);
+
+                int stWater2 = waterStartTime[j];
+                int waterEnd2 = stWater2 + waterDuration[j];
+                int stLand2 = max(waterEnd2 , landStartTime[i]);
+                int endLand2 = stLand2 + landDuration[i];
+                mini = min(mini,endLand2);
+
             }
         }
-
-        return minTime;
+        return mini;
     }
 };
