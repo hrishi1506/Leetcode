8312a04e3a9e0ef50abe1f0f0d28c47bb2027786
Hrishikesh Gurrampally <145208329+hrishi1506@users.noreply.github.com>
2026-06-06 12:13:35 +0530

    Time: 214 ms (64.01%) | Memory: 11.2 MB (22.73%) - LeetSync

---
+++ 3422-find-the-n-th-value-after-k-seconds/find-the-n-th-value-after-k-seconds.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int valueAfterKSeconds(int n, int k) {
+        vector<long long> pref(n,1);
+        long long mod = 1000000007;
+
+        while(k--){
+            for(int i = 1 ; i < n ; i++){
+                pref[i] = (pref[i] + pref[i-1]) % mod;
+            }
+        }
+        return pref[n-1];
+    }
+};
