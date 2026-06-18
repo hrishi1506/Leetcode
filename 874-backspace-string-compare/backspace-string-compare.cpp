de67882f27c14548ce3a4562c9cd93e08fae30c1
Hrishikesh Gurrampally <145208329+hrishi1506@users.noreply.github.com>
2026-06-18 20:42:45 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (64.92%) - LeetSync

--- 874-backspace-string-compare/backspace-string-compare.cpp
+++ 874-backspace-string-compare/backspace-string-compare.cpp
@@ -1,28 +1,85 @@
-class Solution {
+// class Solution {
+// public:
+//     bool backspaceCompare(string s, string t) {
+//         //using stack O(n) EXTRA.. SPACEE..
+
+//         stack<char> st1 , st2;
+
+//         for(char &ch : s){
+//             if(st1.size() > 0 && ch == '#'){
+//                 st1.pop();
+//             }
+//             else if(ch != '#'){
+//                 st1.push(ch);
+//             }
+//         }
+//         for(char &ch : t){
+//             if(st2.size() > 0 && ch == '#'){
+//                 st2.pop();
+//             }
+            
+//             else if(ch != '#'){
+//                 st2.push(ch);
+//             }
+//         }
+//         return st1==st2;
+//     }
+// };
+
+
+class Solution {                         //without extra space;
 public:
     bool backspaceCompare(string s, string t) {
-        //using stack O(n) EXTRA.. SPACEE..
+        int i = s.size()-1;
+        int j = t.size()-1;
 
-        stack<char> st1 , st2;
+        while(i >= 0 || j >= 0){
+            int skipS = 0;
 
-        for(char &ch : s){
-            if(st1.size() > 0 && ch == '#'){
-                st1.pop();
+            while(i >= 0){
+                if(s[i] == '#'){
+                    skipS++;
+                    i--;
+                }
+                else if(skipS > 0){
+                    i--;
+                    skipS--;
+                }
+                else{
+                    break;
+                }
             }
-            else if(ch != '#'){
-                st1.push(ch);
+
+            int skipT = 0;
+            while(j >= 0){
+                if(t[j] == '#'){
+                    skipT++;
+                    j--;
+                }
+                else if(skipT > 0){
+                   
+                    j--;
+                    skipT--;
+                }
+                else {
+                    break;
+                }
             }
-        }
-        for(char &ch : t){
-            if(st2.size() > 0 && ch == '#'){
-                st2.pop();
+
+            if(i >= 0 && j >= 0){
+                if(s[i] != t[j]){
+                    return false;
+                }
             }
+                else if (i >= 0 || j >= 0) {
+                return false;
+            }
+
             
-            else if(ch != '#'){
-                st2.push(ch);
-            }
-        }
-        return st1==st2;
-    }
-};
+            i--;j--;
 
+        }
+
+        return true;
+ }
+};
