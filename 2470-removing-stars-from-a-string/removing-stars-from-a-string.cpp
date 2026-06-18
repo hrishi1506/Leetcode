0b8cbbd6dd194676b1552cd3e70805524966108e
Hrishikesh Gurrampally <145208329+hrishi1506@users.noreply.github.com>
2026-06-18 20:56:08 +0530

    Time: 8 ms (93.11%) | Memory: 27.5 MB (88.67%) - LeetSync

--- 2470-removing-stars-from-a-string/removing-stars-from-a-string.cpp
+++ 2470-removing-stars-from-a-string/removing-stars-from-a-string.cpp
@@ -1,19 +1,47 @@
+// class Solution {
+// public:
+//     string removeStars(string s) {
+//         stack<char>st;
+//         for(char &ch : s){
+//             if(st.size() > 0 && ch == '*') st.pop();
+
+//             else st.push(ch);
+//         }
+
+//         string res = "";
+//         while(!st.empty()){
+//             res += st.top();
+//             st.pop();
+//         }
+//         reverse(res.begin(),res.end());
+//         return res;
+//     }
+// };
+
+
 class Solution {
 public:
     string removeStars(string s) {
-        stack<char>st;
-        for(char &ch : s){
-            if(st.size() > 0 && ch == '*') st.pop();
 
-            else st.push(ch);
-        }
-
+        int cnt = 0;
         string res = "";
-        while(!st.empty()){
-            res += st.top();
-            st.pop();
+       
+        for(int i = s.size()-1 ; i >= 0 ; i--){
+            if(s[i] == '*') {
+                cnt++;
+            }
+            else if(cnt > 0){
+                cnt--;
+            }
+            else{
+                res += s[i];
+            }
         }
+
         reverse(res.begin(),res.end());
+
         return res;
-    }
+
+
+  } 
 };
