17483ef4444744b64e925a478f24cb96f6842437
Hrishikesh Gurrampally <145208329+hrishi1506@users.noreply.github.com>
2026-07-31 09:17:19 +0530

    Time: 9 ms (53.64%) | Memory: 28.3 MB (31.38%) - LeetSync

--- 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.cpp
+++ 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.cpp
@@ -6,30 +6,14 @@ public:
         for(char c : word){
             freq[c -'a']++;
         }
-
-        vector<pair<int,int>> arr;
-
-         for (int i = 0; i < 26; i++) {
-            arr.push_back({freq[i], i}); 
-        }
-       
-        sort(arr.begin(),arr.end() , greater<pair<int,int>>());
+        sort(freq.rbegin(),freq.rend());
 
         int c = 0;
-
         for(int i = 0 ; i < 26 ; i++){
-            if(i < 8){
-                c += arr[i].first;
-            }
-            else if(i < 16){
-                c += arr[i].first*2;
-            }
-            else if(i < 24){
-                c += arr[i].first* 3;
-            }
-            else{
-                c += arr[i].first * 4;
-            }
+            if(freq[i] == 0)
+                break;
+            
+            c += ((i / 8) + 1) * freq[i];
         }
         return c;
     }
