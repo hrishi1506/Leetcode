72d3d6c9788ec0039b56183975cf08a8af4bf02e
Hrishikesh Gurrampally <145208329+hrishi1506@users.noreply.github.com>
2026-08-04 11:39:12 +0530

    Time: 11 ms (6.06%) | Memory: 33.8 MB (17.56%) - LeetSync

--- 4107-find-missing-elements/find-missing-elements.cpp
+++ 4107-find-missing-elements/find-missing-elements.cpp
@@ -6,20 +6,14 @@ public:
         int maxi = *max_element(nums.begin(),nums.end());
         int mini = *min_element(nums.begin(),nums.end());
 
-        unordered_map<int,int>mapp;
+        set<int>st(nums.begin(),nums.end());
 
-        for(int i = mini ; i <= maxi ; i++){
-            mapp[i]++;
-        }   
-        for(int x : nums){
-            mapp[x]++;
-        }
-
-        for(auto &it : mapp){
-            if(it.second == 1){
-                ans.push_back(it.first);
+        for(int i = mini+1 ; i < maxi ; i++){
+            if(st.count(i) == 0){
+                ans.push_back(i);
             }
         }
+
         sort(ans.begin(),ans.end());
         return ans;
     }
